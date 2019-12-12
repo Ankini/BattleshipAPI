9b357b44234d0df0ced280ceb25b77e796dbb60c
Ankinihvyas <ankinihjoshi@gmail.com>
2019-12-12 13:35:17 +1100

    Rename Service folder
    Signed-off-by: Ankinihvyas <ankinihjoshi@gmail.com>

---
+++ Battleship.Services/Filters/ApiExceptionResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace Battleship.Services.Filters
+{
+    //Exception filters can be used for handling expected exceptions
+    //For Expected excception, we can format the response as per the context
+
+    public class ApiExceptionResponse : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (context.Exception is NotImplementedException)
+            {
+                context.Response = new HttpResponseMessage(HttpStatusCode.NotImplemented);
+            }
+        }
+    }
+}
---
+++ Battleship.Services/Filters/GlobalExceptionLogger.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+
+namespace Battleship.Services.Filters
+{
+    /// <summary>
+    /// Logging all the unhandled exception for health monitoring and trouble shooting
+    /// </summary>
+    class GlobalExceptionLogger : IExceptionLogger
+    {
+        public Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+        {
+            var exception = context.ExceptionContext.Exception;
+            //TODO: Implement the proper logging solution
+            //Log the exception as per the logging implementation for the application
+
+            return Task.FromResult(0);
+        }
+    }
+}
